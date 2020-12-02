3495d33fb4d949de3600abd96ea43c1d0ae8dd8c
viktarstaselovich <staselovich.vitya@gmail.com>
2020-12-02 21:18:33 +0300

    section about us + simplebar for section hero

--- js/burger.js
+++ js/burger.js
@@ -2,4 +2,5 @@ window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#menu').classList.toggle('is-active')
   })
+
 })
