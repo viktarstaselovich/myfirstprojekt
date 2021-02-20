3f6b157332b1fc1e6464b52481814e4e6a26e1fa
viktarstaselovich <staselovich.vitya@gmail.com>
2021-02-20 23:34:22 +0300

    new type

--- js/burger.js
+++ js/burger.js
@@ -2,5 +2,8 @@ window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#menu').classList.toggle('is-active')
   })
+  document.querySelector('#search-form').addEventListener('click', function() {
+    document.querySelector('#search-block').classList.toggle('is-active')
+  })
 
 })
